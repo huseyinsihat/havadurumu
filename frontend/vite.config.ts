4af0dcbc7e7705c1863d9e62db78a8573cbfc024
Hüseyin SIHAT <huseyin.sihat@gazi.edu.tr>
2026-02-06 12:42:06 +0300

    fix: TypeScript error - unused env variable

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  // .env.production, .env.development dosyaları otomatik load olsun
+  loadEnv(mode, process.cwd(), '')
   
   return {
     plugins: [react()],
